678f9f1f40ee73a5b1e481985e0a03dda362ab3e
HydrolienF <hydrolien.f@gmail.com>
2020-11-16 10:14:53 +0100

    add some comment

--- src/main/fr/formiko/formiko/interfaces/TrophallaxieNull.java
+++ src/main/fr/formiko/formiko/interfaces/TrophallaxieNull.java
@@ -16,15 +16,27 @@ public class TrophallaxieNull implements Serializable, Trophallaxie {
   /*public void trophallaxieIa(Creature c1, Creature c2){
     erreur.erreur("Impossible de trophalaxer avec la créature " + c1.getId());
   }*/
+  /**
+  *If lauch print an error.
+  */
   public void trophallaxie(Creature c, Creature c2, int x){
     erreur.erreur("Impossible de trophalaxer avec la créature " + c.getId());
   }
+  /**
+  *If lauch print an error.
+  */
   public void trophallaxie(Creature c, int id, int x){
     trophallaxie(c,(Creature) null, -1);
   }
+  /**
+  *If lauch print an error.
+  */
   public void trophallaxer(Creature c){
     trophallaxie(c,(Creature) null, -1);
   }
+  /**
+  *If lauch print an error.
+  */
   public int [] getCreatureQuiOnFaim(int t [], Creature c){
     trophallaxie(c,(Creature) null, -1);
     return null;
