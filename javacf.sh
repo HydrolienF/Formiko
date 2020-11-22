5ebc68dad534fa1241e01c0465fa3b03d5d0a205
HydrolienF <hydrolien.f@gmail.com>
2020-11-22 15:06:35 +0100

    large file .zip should be stored in gfs & junit file for test is now in the git repository

--- javacf.sh
+++ javacf.sh
@@ -1,3 +1,3 @@
 #command to javac main file.
 #exemple of use : ./javacf.sh
-javac -d build/main/ -cp ~/Formiko/junit-4.13.1.jar:. $(find src/main -name *.java)
+javac -d build/main/ -cp junit-4.13.1.jar:. $(find src/main -name *.java)
