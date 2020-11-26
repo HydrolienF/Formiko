08321511356dc5be9b1462fdea138c87924978b5
HydrolienF <hydrolien.f@gmail.com>
2020-11-26 13:12:41 +0100

    javadoc now use juint file from git repository

--- javadocf.sh
+++ javadocf.sh
@@ -1,1 +1,1 @@
-javadoc -author -private -d docs/javadoc/ -cp ~/Formiko/junit-4.13.1.jar:. $(find src/main -name *.java)
+javadoc -author -private -d docs/javadoc/ -cp ./junit-4.13.1.jar:. $(find src/main -name *.java)
