3190948152da9776374b24b65b79c71972e1d952
HydrolienF <hydrolien.f@gmail.com>
2020-11-26 13:38:47 +0100

    javadoc is quiet

--- javadoc.sh
+++ javadoc.sh
@@ -1,3 +1,3 @@
 #Generate the javadoc for the project in docs/javadoc
 
-javadoc -author -private -d docs/javadoc/ -cp ./junit-4.13.1.jar:. $(find src/main -name *.java)
+javadoc -quiet -author -private -d docs/javadoc/ -cp ./junit-4.13.1.jar:. $(find src/main -name *.java)
