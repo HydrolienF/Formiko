f47a7571862ed1f372635d59ad025db80df40d7c
HydrolienF <hydrolien.f@gmail.com>
2021-01-12 10:18:15 +0100

    compterChaquePixel is tested to

--- src/test/fr/formiko/usuel/ImgTest.java
+++ src/test/fr/formiko/usuel/ImgTest.java
@@ -6,6 +6,7 @@ import fr.formiko.usuel.test.TestCaseMuet;
 import org.junit.Test;
 import fr.formiko.usuel.tableau;
 import fr.formiko.usuel.image.Pixel;
+import java.util.HashMap;
 
 public class ImgTest extends TestCaseMuet{
 
@@ -215,6 +216,29 @@ public class ImgTest extends TestCaseMuet{
     assertEquals(1,img.compterPixel(p));
   }
   public void testCompterChaquePixel(){
-
+    Img img = new Img(2,2);
+    Pixel p=new Pixel(128,128,128,128);
+    HashMap hm = img.compterChaquePixel();
+    Object to[] = hm.keySet().toArray();
+    Pixel tp[] = new Pixel[to.length];
+    for (int i=0;i<to.length ;i++ ) {
+      tp[i] = (Pixel)to[i];
+    }
+    assertEquals(1,tp.length);
+    assertEquals(4,hm.get(tp[0]));
+    byte tb [][] = new byte[2][2];
+    tb[0][0]=(byte)1;tb[0][1]=(byte)1;tb[1][0]=(byte)1;tb[1][1]=(byte)2;
+    img.setRouge(tb);
+    hm = img.compterChaquePixel();
+    to = hm.keySet().toArray();
+    tp = new Pixel[to.length];
+    for (int i=0;i<to.length ;i++ ) {
+      tp[i] = (Pixel)to[i];
+    }
+    assertEquals(2,tp.length);
+    assertEquals(3,hm.get(tp[0]));
+    assertEquals(1,hm.get(tp[1]));
+    assertTrue(tp[0].equals(new Pixel(129,128,128,128)));
+    assertTrue(tp[1].equals(new Pixel(130,128,128,128)));
   }
 }
