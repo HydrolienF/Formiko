57390e3db4397ee5b8b8826c6f0faafdfe17160c
HydrolienF <hydrolien.f@gmail.com>
2021-02-14 13:28:22 +0100

    new taille réaliste

--- src/main/java/fr/formiko/usuel/chargerLesOptions.java
+++ src/main/java/fr/formiko/usuel/chargerLesOptions.java
@@ -115,7 +115,7 @@ public class chargerLesOptions {
     son:0
     volMusique:100
     volSon:100
-    tailleRéaliste:40
+    tailleRéaliste:30
     """);
     //TODO la musique est temporairement désactivé. La réactivé quand elle marchera.
     ecrireUnFichier.ecrireUnFichier(gs,"data/Options.txt");
