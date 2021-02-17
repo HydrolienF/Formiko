b7bd74e28e0eac5d61149a064d3f373b531b8702
HydrolienF <hydrolien.f@gmail.com>
2021-02-17 15:32:56 +0100

    minor change for jpackage

--- exe.sh
+++ exe.sh
@@ -36,15 +36,7 @@ cp README.md out/$nom/.
 cd out/$nom
 
 #suppress all file that will be recreate on the computer of the user.
-echo "Suppress unuse fill from data"
-rm -fr data/Options.txt
-rm -f data/score*
-rm -f data/.~*
-rm -f data/.save
-rm -f data/Temps.txt
-rm -fr data/sauvegarde/*
-rm -fr data/image/temporaire/*
-rm -fr data/image/ressourcesPack/*
+./clearData.sh
 
 cd ../..
 mkdir out/$nomW
