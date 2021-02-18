ba1346079b8baeb671aa2c00978b95ff6a541432
HydrolienF <hydrolien.f@gmail.com>
2021-02-18 08:50:26 +0100

    #201 done (ViewNull)

---
+++ src/main/java/fr/formiko/views/ViewNull.java
@@ -0,0 +1,73 @@
+package fr.formiko.views;
+
+/**
+ *{@summary Null view. A simple view who do nothing when a view action is launch.}<br>
+ *@author Hydrolien
+ *@version 1.33
+ */
+public class ViewNull {
+  /**
+  *{@summary A null view action that do nothing.}<br>
+  *@return Return true if nothing have been print or paint.
+  *@version 1.33
+  */
+  public boolean ini(){
+    return true;
+  }
+  /**
+  *{@summary A null view action that do nothing.}<br>
+  *@return Return true if nothing have been print or paint.
+  *@version 1.33
+  */
+  public boolean paint(){
+    return true;
+  }
+  /**
+  *{@summary A null view action that do nothing.}<br>
+  *@return Return true if nothing have been print or paint.
+  *@version 1.33
+  */
+  public boolean menuMain(){
+    return true;
+  }
+  /**
+  *{@summary A null view action that do nothing.}<br>
+  *@return Return true if nothing have been print or paint.
+  *@version 1.33
+  */
+  public boolean menuLoadAGame(){
+    return true;
+  }
+  /**
+  *{@summary A null view action that do nothing.}<br>
+  *@return Return true if nothing have been print or paint.
+  *@version 1.33
+  */
+  public boolean actionGame(){
+    return true;
+  }
+  /**
+  *{@summary A null view action that do nothing.}<br>
+  *@return Return true if nothing have been print or paint.
+  *@version 1.33
+  */
+  public boolean pauseActionGame(){
+    return true;
+  }
+  /**
+  *{@summary A null view action that do nothing.}<br>
+  *@return Return true if nothing have been print or paint.
+  *@version 1.33
+  */
+  public boolean setPlayingAnt(){
+    return true;
+  }
+  /**
+  *{@summary A null view action that do nothing.}<br>
+  *@return Return true if nothing have been print or paint.
+  *@version 1.33
+  */
+  public boolean setLookedCase(){
+    return true;
+  }
+}
