b3981e209b6f76b5b6710ada2c7e39fd4ac90460
HydrolienF <hydrolien.f@gmail.com>
2021-03-25 13:44:19 +0100

    [#69] more comment.

--- src/main/java/fr/formiko/usuel/color.java
+++ src/main/java/fr/formiko/usuel/color.java
@@ -1,11 +1,19 @@
 package fr.formiko.usuel;
 
+/**
+*{@summary Color used by linux console.}<br>
+*All String can be call by color.NAME.<br>
+*To set back to normal color use color.NEUTRAL.<br>
+*@author Hydrolien
+*@version 1.39
+*/
 public class color {
   //colors
+  public static String NEUTRAL = (char)27+"[0;m";
+
   public static String RED = (char)27+"[1;31m";
   public static String YELLOW = (char)27+"[1;33m";
   public static String GREEN = (char)27+"[1;32m";
-  public static String NEUTRAL = (char)27+"[0;m";
   public static String WHITE = (char)27+"[1;37m";
   public static String BROWN = (char)27+"[0;m";
   public static String BLACK = (char)27+"[1;30m";
