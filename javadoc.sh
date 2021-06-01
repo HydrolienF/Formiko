c99af0c4265fb25b7606a534fe1bc42a06bef4fb
HydrolienF <hydrolien.f@gmail.com>
2021-06-01 09:26:01 +0200

    Update style.css to use javadoc 1 & our 1.

--- javadoc.sh
+++ javadoc.sh
@@ -1,13 +1,10 @@
 #Generate the javadoc for the project in docs/javadoc
 
 javadoc -doctitle "Javadoc of Formiko" -version -quiet -author -private -d docs/javadoc/ $(find src/main/java -name "*.java" | grep -v fr/formiko/usuel/tests/) -locale en_US -stylesheetfile docs/javadoc/styles.css -header '
-<h1 id="main-title">Formiko</h1>
-<nav>
-  <ul id="buttons-bar">
-    <li id="button-home-page"><a href="./index.html">Home</a></li>
-    <li id="button-download-page"><a href="./download.html">Download</a></li>
-    <li id="button-github-page"><a href="https://github.com/HydrolienF/Formiko/">Github</a></li>
-    <li id="button-javadoc-page"><a href="./Formiko/javadoc/index.html">Javadoc</a></li>
-  </ul>
-</nav>'
+<ul id="buttons-bar">
+  <li class="mainButon" id="button-home-page"><a href="./index.html">Home</a></li>
+  <li class="mainButon" id="button-download-page"><a href="./download.html">Download</a></li>
+  <li class="mainButon" id="button-github-page"><a href="https://github.com/HydrolienF/Formiko/">Github</a></li>
+  <li class="mainButon" id="button-javadoc-page"><a href="./Formiko/javadoc/index.html">Javadoc</a></li>
+</ul>'
 # -bottom
