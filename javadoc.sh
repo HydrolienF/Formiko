e873f35622e0e5556fab2f4ae28368dc150413e1
HydrolienF <hydrolien.f@gmail.com>
2021-06-01 09:26:50 +0200

    fix link for javadoc

--- javadoc.sh
+++ javadoc.sh
@@ -2,9 +2,9 @@
 
 javadoc -doctitle "Javadoc of Formiko" -version -quiet -author -private -d docs/javadoc/ $(find src/main/java -name "*.java" | grep -v fr/formiko/usuel/tests/) -locale en_US -stylesheetfile docs/javadoc/styles.css -header '
 <ul id="buttons-bar">
-  <li class="mainButon" id="button-home-page"><a href="./index.html">Home</a></li>
-  <li class="mainButon" id="button-download-page"><a href="./download.html">Download</a></li>
+  <li class="mainButon" id="button-home-page"><a href="https://formiko.fr/">Home</a></li>
+  <li class="mainButon" id="button-download-page"><a href="https://formiko.fr/download.html">Download</a></li>
   <li class="mainButon" id="button-github-page"><a href="https://github.com/HydrolienF/Formiko/">Github</a></li>
-  <li class="mainButon" id="button-javadoc-page"><a href="./Formiko/javadoc/index.html">Javadoc</a></li>
+  <li class="mainButon" id="button-javadoc-page"><a href="https://formiko.fr/Formiko/javadoc/index.html">Javadoc</a></li>
 </ul>'
 # -bottom
