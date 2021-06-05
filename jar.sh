579737db1ec661584c681a1c86e07a1730edd132
HydrolienF <hydrolien.f@gmail.com>
2021-06-05 13:45:58 +0200

    -ntp

--- jar.sh
+++ jar.sh
@@ -1,5 +1,5 @@
 #create a jar file.
 #exemple of use : ./jar.sh Formiko
 # cd target/classes/.;jar cfm $1.jar ../../manifest.txt *;mv $1.jar ../../.;cd ../..
-mvn versions:set -DnewVersion="$(cat version.md)"; mvn package
+mvn -ntp versions:set -DnewVersion="$(cat version.md)"; mvn -ntp package
 mv target/Formiko-$(cat version.md)-jar-with-dependencies.jar Formiko.jar
