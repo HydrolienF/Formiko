5073df37e67ea98fc6b1255752fb37b78806ea41
HydrolienF <hydrolien.f@gmail.com>
2021-06-10 11:35:43 +0200

    jar.sh update

--- jar.sh
+++ jar.sh
@@ -1,5 +1,8 @@
 #create a jar file.
-#exemple of use : ./jar.sh Formiko
+#exemple of use : ./jar.sh
 # cd target/classes/.;jar cfm $1.jar ../../manifest.txt *;mv $1.jar ../../.;cd ../..
-mvn -ntp versions:set -DnewVersion="$(cat version.md)"; mvn -ntp package
+mvn -ntp versions:set -DnewVersion="$(cat version.md)"
+# rm src/main/resources/version.json
+# cp version.json src/main/resources/
+mvn -ntp package
 mv target/Formiko-$(cat version.md)-jar-with-dependencies.jar Formiko.jar
