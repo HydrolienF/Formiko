b3fd2aead52567821dd829356243aa10a7754de3
HydrolienF <hydrolien.f@gmail.com>
2021-10-23 16:46:08 +0200

    get branch from getLastBranch.sh

--- countCommitFromLastBranch.sh
+++ countCommitFromLastBranch.sh
@@ -1,5 +1,5 @@
 # branchVersion=$(git symbolic-ref HEAD --short)
-branchVersion=$(git branch --format="%(refname)" | grep 2. | tail -q -n 1 | tail -c +12)
+branchVersion=$(./getLastBranch.sh)
 branchLastVersion=""
 k=true;
 for i in `echo $branchVersion | tr "." " "`; do
