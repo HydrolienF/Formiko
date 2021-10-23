3de8b82362c44d90669f9b9ae016660799dc7dfa
HydrolienF <hydrolien.f@gmail.com>
2021-10-23 13:10:51 +0200

    More print

--- countCommitFromLastBranch.sh
+++ countCommitFromLastBranch.sh
@@ -10,7 +10,10 @@ for i in `echo $branchVersion | tr "." " "`; do
     branchLastVersion=$branchLastVersion"."$(($i-1))
   fi
 done
-echo $branchLastVersion-$branchVersion
+echo git branch
+echo ""
+echo $branchLastVersion
+echo $branchVersion
 totalCommit=$(git rev-list --count $branchVersion)
 lastBranchTotalCommit=$(git rev-list --count $branchLastVersion)
 curentBranchCommit=$(($totalCommit-$lastBranchTotalCommit))
