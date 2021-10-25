533ef3cd9fbdf9e918864c201f51853c9bd79f9c
HydrolienF <hydrolien.f@gmail.com>
2021-10-25 09:30:47 +0200

    fix countCommitfromLastBranch.sh

--- countCommitFromLastBranch.sh
+++ countCommitFromLastBranch.sh
@@ -1,8 +1,9 @@
 # branchVersion=$(git symbolic-ref HEAD --short)
+# | head -c -2 remove the weard special char at the end of lastBranch.
 branchVersion=$(./getLastGitBranch.sh)
 branchLastVersion=""
 k=true;
-for i in 'echo $branchVersion | tr "." " "'; do
+for i in `echo $branchVersion | tr "." " "`; do
   if [ $k = true ]; then
     k=false
     branchLastVersion=$i
@@ -10,8 +11,8 @@ for i in 'echo $branchVersion | tr "." " "'; do
     branchLastVersion=$branchLastVersion"."$(($i-1))
   fi
 done
-echo $branchLastVersion
-echo $branchVersion
+# echo $branchLastVersion
+# echo $branchVersion
 totalCommit=$(git rev-list --count $branchVersion)
 lastBranchTotalCommit=$(git rev-list --count $branchLastVersion)
 curentBranchCommit=$(($totalCommit-$lastBranchTotalCommit))
