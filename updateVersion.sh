cc6f828902f40feced009d549c953ecd02905160
HydrolienF <hydrolien.f@gmail.com>
2021-10-27 11:57:37 +0200

    Remove unuse .sh file.

--- updateVersion.sh
+++
@@ -1,4 +0,0 @@
-curentBranchCommit=$(./countCommitFromLastBranch.sh)
-# branchVersion=$(git symbolic-ref HEAD --short)
-branchVersion=$(./getLastGitBranch.sh)
-echo $branchVersion'.'$curentBranchCommit > version.md
