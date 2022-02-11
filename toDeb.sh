1b710a0c4f61228d891f68b69a390f055e72cb9f
HydrolienF <hydrolien.f@gmail.com>
2022-02-11 18:08:30 +0100

    Add installer

--- toDeb.sh
+++ toDeb.sh
@@ -1,4 +1,5 @@
 # sudo apt-get install ./formiko_2.19.39-1_amd64.deb
+# sudo apt remove formiko
 ./updateVersion.sh > version.md
 mvn -ntp versions:set -DnewVersion="$(cat version.md)"
-jpackage --input in -d out --name Formiko --main-jar Formiko.jar --main-class fr.formiko.formiko.Main --app-version $(cat version.md) --description "Formiko, a game about ant." --icon tools/icon.png --runtime-image jlink/jLinux --license-file LICENSE.md --vendor Hydrolien -t deb --linux-package-name formiko --linux-shortcut
+jpackage create-installer --input in -d out --name Formiko --main-jar Formiko.jar --main-class fr.formiko.formiko.Main --app-version $(cat version.md) --description "Formiko, a game about ant." --icon tools/icon.png --runtime-image jlink/jLinux --license-file LICENSE.md --vendor Hydrolien -t deb --linux-package-name formiko --linux-shortcut
