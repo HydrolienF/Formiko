4bc3e863c7cd86fe374ac129b03b186b8461e1cc
HydrolienF <hydrolien.f@gmail.com>
2022-02-11 18:09:55 +0100

    add runtime installer

--- toDeb.sh
+++ toDeb.sh
@@ -2,4 +2,4 @@
 # sudo apt remove formiko
 ./updateVersion.sh > version.md
 mvn -ntp versions:set -DnewVersion="$(cat version.md)"
-jpackage create-installer --input in -d out --name Formiko --main-jar Formiko.jar --main-class fr.formiko.formiko.Main --app-version $(cat version.md) --description "Formiko, a game about ant." --icon tools/icon.png --runtime-image jlink/jLinux --license-file LICENSE.md --vendor Hydrolien -t deb --linux-package-name formiko --linux-shortcut
+jpackage create-installer --runtime-installer --input in -d out --name Formiko --main-jar Formiko.jar --main-class fr.formiko.formiko.Main --app-version $(cat version.md) --description "Formiko, a game about ant." --icon tools/icon.png --runtime-image jlink/jLinux --license-file LICENSE.md --vendor Hydrolien -t deb --linux-package-name formiko --linux-shortcut
