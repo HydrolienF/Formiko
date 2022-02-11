b5c5b0daa160b27fa7e0b366407b9aed5928bed2
HydrolienF <hydrolien.f@gmail.com>
2022-02-11 18:18:00 +0100

    Add way to launch on linux from deb.

--- toDeb.sh
+++ toDeb.sh
@@ -1,5 +1,7 @@
 # sudo apt-get install ./formiko_2.19.39-1_amd64.deb
 # sudo apt remove formiko
+# cd /opt/formiko/bin/
+# ./Formiko
 ./updateVersion.sh > version.md
 mvn -ntp versions:set -DnewVersion="$(cat version.md)"
-jpackage create-installer --runtime-installer --input in -d out --name Formiko --main-jar Formiko.jar --main-class fr.formiko.formiko.Main --app-version $(cat version.md) --description "Formiko, a game about ant." --icon tools/icon.png --runtime-image jlink/jLinux --license-file LICENSE.md --vendor Hydrolien -t deb --linux-package-name formiko --linux-shortcut
+jpackage --input in -d out --name Formiko --main-jar Formiko.jar --main-class fr.formiko.formiko.Main --app-version $(cat version.md) --description "Formiko, a game about ant." --icon tools/icon.png --runtime-image jlink/jLinux --license-file LICENSE.md --vendor Hydrolien -t deb --linux-package-name formiko --linux-shortcut
