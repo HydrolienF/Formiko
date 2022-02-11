eacd9a3eb164a5b3cdb533bbaa3e0a3e745a6b9e
HydrolienF <hydrolien.f@gmail.com>
2022-02-11 17:55:21 +0100

    New try to create .deb

---
+++ toDeb.sh
@@ -0,0 +1,4 @@
+./updateVersion.sh > version.md
+mvn -ntp versions:set -DnewVersion="$(cat version.md)"
+# jpackage --input in -d out --name Formiko --main-jar Formiko.jar --main-class fr.formiko.formiko.Main --app-version $(cat version.md) --description "Formiko, a game about ant." --icon tools/icon.png --runtime-image jlink/jWindows --license-file LICENSE.md --vendor Hydrolien -t deb
+jpackage --input in -d out --name Formiko --main-jar Formiko.jar --main-class fr.formiko.formiko.Main --app-version $(cat version.md) --description "Formiko, a game about ant." --icon tools/icon.png --runtime-image jlink/jLinux --license-file LICENSE.md --vendor Hydrolien -t deb --linux-package-name formiko
