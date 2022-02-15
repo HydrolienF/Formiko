cc0b55c6c47c6d50957ee7766b3a55033cde938c
HydrolienF <hydrolien.f@gmail.com>
2022-02-15 09:31:41 +0100

    Update toDeb with full op to have .jar

--- toDeb.sh
+++ toDeb.sh
@@ -1,13 +1,23 @@
 # sudo apt-get install ./formiko_2.19.39-1_amd64.deb
+# sudo apt-get install ./out/formiko_2.19.46-1_amd64.deb
 # sudo apt remove formiko
 # cd /opt/formiko/bin/
 # ./Formiko
 ./updateVersion.sh > version.md
+# Start of common part with toMsi
 mvn -ntp versions:set -DnewVersion="$(cat version.md)"
-# generate jar
-# mv Formiko.jar in/
+rm src/main/resources/res/version.json
+cp version.json src/main/resources/res/
+mvn -ntp compile
+mvn -ntp package
+mv target/Formiko-$(cat version.md)-jar-with-dependencies.jar Formiko.jar
+
+rm -fr in
+mkdir in
+mv Formiko.jar in/
 cp LICENSE.md in/
 cp README.md in/
 cp version.md in/
 cp version.json in/
+# End of common part with toMsi
 jpackage --input in -d out --name Formiko --main-jar Formiko.jar --main-class fr.formiko.formiko.Main --app-version $(cat version.md) --description "Formiko, a game about ant." --icon tools/icon.png --runtime-image jlink/jLinux --license-file LICENSE.md --vendor Hydrolien -t deb --linux-package-name formiko --linux-shortcut
