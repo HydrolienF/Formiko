cdf75f4b917d200fa0bc591ac6d3cc6bf1f119a9
HydrolienF <hydrolien.f@gmail.com>
2022-02-15 09:11:04 +0100

    Add LICENSE, README, version.

--- toDeb.sh
+++ toDeb.sh
@@ -4,4 +4,10 @@
 # ./Formiko
 ./updateVersion.sh > version.md
 mvn -ntp versions:set -DnewVersion="$(cat version.md)"
+# generate jar
+# mv Formiko.jar in/
+cp LICENSE.md in/
+cp README.md in/
+cp version.md in/
+cp version.json in/
 jpackage --input in -d out --name Formiko --main-jar Formiko.jar --main-class fr.formiko.formiko.Main --app-version $(cat version.md) --description "Formiko, a game about ant." --icon tools/icon.png --runtime-image jlink/jLinux --license-file LICENSE.md --vendor Hydrolien -t deb --linux-package-name formiko --linux-shortcut
