e3e2f2c7d8c2e23dd9eed013581a5317f08c4ad0
HydrolienF <hydrolien.f@gmail.com>
2022-03-10 14:49:35 +0100

    pass agrs to .sh launcher.

--- launcher/formiko.sh
+++ launcher/formiko.sh
@@ -1,1 +1,1 @@
-echo game launch on Linux; chmod 700 java/bin/java;./java/bin/java -XX:SoftMaxHeapSize=2G -XX:MaxRAMPercentage=90 -jar Formiko.jar;
+echo game launch on Linux; chmod 700 java/bin/java;./java/bin/java -XX:SoftMaxHeapSize=2G -XX:MaxRAMPercentage=90 -jar Formiko.jar $@;
