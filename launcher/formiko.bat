4504c2870f1eb9678d4b9461fd7d536626084b78
HydrolienF <hydrolien.f@gmail.com>
2022-03-10 15:19:26 +0100

    Use ZGC that need less memory & should stop at 2GB if it can, or at 90% of memory.

--- launcher/formiko.bat
+++ launcher/formiko.bat
@@ -1,3 +1,3 @@
 @echo off
 echo game launch on Windows
-"java/bin/java.exe" -XX:SoftMaxHeapSize=2G -XX:MaxRAMPercentage=90 -jar Formiko.jar
+"java/bin/java.exe" -XX:+UseZGC -XX:SoftMaxHeapSize=2G -XX:MaxRAMPercentage=90 -jar Formiko.jar
