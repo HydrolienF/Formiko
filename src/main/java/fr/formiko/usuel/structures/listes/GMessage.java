f7085a258859c4443809c6c64cf63395f32ed67a
HydrolienF <hydrolien.f@gmail.com>
2022-03-31 16:18:59 +0200

    [#82] doing GMessage.

--- src/main/java/fr/formiko/usuel/structures/listes/GMessage.java
+++ src/main/java/fr/formiko/usuel/structures/listes/GMessage.java
@@ -5,51 +5,20 @@ import fr.formiko.usuel.g;
 
 import java.io.Serializable;
 
-public class GMessage implements Serializable{
-  private CMessage début, fin;
-  // CONSTRUCTORS --------------------------------------------------------------
-  public GMessage(){}
-  // GET SET -------------------------------------------------------------------
-  public CMessage getHead(){ return début;}
-  public CMessage getTail(){ return fin;}
+public class GMessage extends Liste<Message> implements Serializable {
   // FUNCTIONS -----------------------------------------------------------------
-  public int length(){
-    if(début==null){ return 0;}
-    return début.length();
-  }
-  public void add(Message s){ // On ajoute a la fin par défaut.
-    CMessage c = new CMessage(s);
-    if (fin == null){ // si la liste est complètement vide.
-      fin = c;
-      début = c;
-    } else {
-      fin.setSuivant(c);
-      c.setPrécédent(fin);
-      fin = c;
-    }
-  }
-  public void add(GMessage gs){
-    if(gs==null){ return;}
-    if(this.getHead()==null){début = gs.getHead();return;}
-    //on lie l'anciène fin au début de gs.
-    this.fin.setSuivant(gs.getHead());
-    this.fin.getSuivant().setPrécédent(fin);
-    // on change la fin actuelle.
-    this.fin = gs.getTail();
-  }
-  public void afficheToi(){
-    if(début==null){
-      System.out.println(g.get("GMessage",1,"Le GMessage est vide"));
-    }else{
-      début.afficheTout();
-    }
-  }
-  public boolean contient(Message s){
-    if (début==null){ return false;}
-    return début.contient(s);
-  }
   public GString gmToGs(int x){
-    if (début==null){ return new GString();}
-    return fin.gmToGs(x);
+    //TODO
+    return null;
+    // if (début==null){ return new GString();}
+    // return fin.gmToGs(x);
+    // while(x>0 && csTemp != null){
+    //   gs.add(csTemp.getContent().description());
+    //   csTemp = csTemp.getPrécédent();
+    //   x--;
+    //   if(x==0){
+    //     gs.addTail("...");
+    //   }
+    // }
   }
 }
