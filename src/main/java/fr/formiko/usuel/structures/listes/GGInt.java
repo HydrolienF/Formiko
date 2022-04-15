86d49db46114a8cb3faad0c9bb15cb14725970ab
HydrolienF <hydrolien.f@gmail.com>
2022-04-15 11:02:36 +0200

    clean file.

--- src/main/java/fr/formiko/usuel/structures/listes/GGInt.java
+++ src/main/java/fr/formiko/usuel/structures/listes/GGInt.java
@@ -31,45 +31,4 @@ public class GGInt extends Liste<GInt> implements Serializable {
     if(total<-1){total=-1;}// -1 = at least 1 error.
     return total;
   }
-
-  /*
-  private CCInt début, fin;
-  // CONSTRUCTORS ----------------------------------------------------------------
-  public GGInt(){}
-  // GET SET ----------------------------------------------------------------------
-  public CCInt getHead(){ return début;}
-  public CCInt getTail(){ return fin;}
-  // FUNCTIONS -----------------------------------------------------------------
-  public int length(){
-    if(début==null){ return 0;}
-    return début.length();
-  }
-  public void add(GInt contenu){ // On ajoute a la fin par défaut.
-    CCInt c = new CCInt(contenu);
-    if (fin == null){ // si la liste est complètement vide.
-      fin = c;
-      début = c;
-    } else {
-      fin.setSuivant(c);
-      c.setPrécédent(fin);
-      fin = c;
-    }
-  }
-  public void afficheToi(){
-    if(début==null){
-      System.out.println(g.get("GGInt",1,"Le GGInt est vide"));
-    }else{
-      début.afficheTout();
-    }
-  }
-  public String toString(){
-    if(début==null){return "";}
-    else{return début.toString();}
-  }
-  public int sommeCase(int ca){
-    if(ca<0){return -1;}
-    if(début==null){return 0;}
-    else{return début.sommeCase(ca);}
-  }
-  */
 }
