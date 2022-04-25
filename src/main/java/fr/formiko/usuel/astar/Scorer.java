1014ce8958cd00e4a76c3071b25124c326ba05bd
HydrolienF <hydrolien.f@gmail.com>
2022-04-25 15:56:07 +0200

    [#467] Add the implementation of A* by Baeldung.

---
+++ src/main/java/fr/formiko/usuel/astar/Scorer.java
@@ -0,0 +1,11 @@
+package fr.formiko.usuel.astar;
+
+/**
+*{@summary Define a simple Scorer.}
+*taken from https://github.com/eugenp/tutorials/tree/master/algorithms-miscellaneous-2/src/test/java/com/baeldung/algorithms/astar
+*@author sazzer
+*@lastEditedVersion 2.23
+*/
+public interface Scorer<T extends GraphNode> {
+  double computeCost(T from, T to);
+}
